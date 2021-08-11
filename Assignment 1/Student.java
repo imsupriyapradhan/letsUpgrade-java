1c19bc61bbca3bb99bda78ebc49a9caa9e07920d
imsupriyapradhan <87229326+imsupriyapradhan@users.noreply.github.com>
2021-08-11 00:33:29 -0700

    Assignment-1(Day1)

---
+++ Assignment 1/Student.java
@@ -0,0 +1,11 @@
+
+public class Student {
+	public static void main(String[] args) {
+		
+		int rollNumber = 35;
+		String name = "Jasmine";
+		float marks = 85.6f;
+		System.out.print("Roll Number: " + rollNumber + "\n" + "Name: " + name + "\n" + "Marks: " +marks);
+	}
+
+}
